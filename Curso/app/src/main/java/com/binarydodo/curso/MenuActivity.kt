8723edc9f9c05846aafe142b2a1a8f6346c30961
David Puga Gallego <davidpugagallego@gmail.com>
2025-06-05 16:43:08 +0200

    Cambios

--- Curso/app/src/main/java/com/binarydodo/curso/MenuActivity.kt
+++ Curso/app/src/main/java/com/binarydodo/curso/MenuActivity.kt
@@ -1,10 +1,15 @@
 package com.binarydodo.curso
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.binarydodo.curso.firstapp.FirstAppActivity
+import com.binarydodo.curso.imccalculator.imcCalculatorActivity
+
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,19 @@ class MenuActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        val btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
+        btnSaludApp.setOnClickListener{navigateToSaludApp()}
+        btnIMCApp.setOnClickListener{navigateToIMCApp()}
+    }
+
+    private fun navigateToIMCApp() {
+        val intent = Intent(this, imcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToSaludApp(){
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
     }
 }
--- Curso/app/src/main/java/com/binarydodo/curso/imccalculator/imcCalculatorActivity.kt
+++ Curso/app/src/main/java/com/binarydodo/curso/imccalculator/imcCalculatorActivity.kt
@@ -17,5 +17,7 @@ class imcCalculatorActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
